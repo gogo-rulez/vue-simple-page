bd5304f1d85ab369d8ecf89d896c41c32076b701
goran.savic <goran.savic@styria.hr>
2021-03-03 12:35:56 +0100

    deploy

---
+++ precache-manifest.051db066a69506aced45125ba7edc6e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d116607d066453627e",
+    "url": "/vue-simple-page/css/app.225c1c52.css"
+  },
+  {
+    "revision": "289d9df3106d2c72dcbd92ec304f978a",
+    "url": "/vue-simple-page/img/homepage.289d9df3.jpg"
+  },
+  {
+    "revision": "2a11f3ed5b2f38ad71baeda15fb3b30c",
+    "url": "/vue-simple-page/index.html"
+  },
+  {
+    "revision": "35d116607d066453627e",
+    "url": "/vue-simple-page/js/app.a8ef0ebf.js"
+  },
+  {
+    "revision": "7a58971666a77eb08a51",
+    "url": "/vue-simple-page/js/chunk-vendors.f556b742.js"
+  },
+  {
+    "revision": "1c4ec4c11db9418f7d1c09e969d5aa42",
+    "url": "/vue-simple-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-simple-page/robots.txt"
+  }
+]);
