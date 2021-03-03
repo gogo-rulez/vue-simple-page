906d7470a89370aa98cc1c34f4f8c84cbf7b03c0
goran.savic <goran.savic@styria.hr>
2021-03-03 09:40:52 +0100

    add header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
     <div id="app">
+
+        <site-header />
+
     </div>
 </template>
 
+<script>
+import SiteHeader from '@/components/SiteHeader';
+
+export default {
+    name: 'App',
+
+    components: {
+        SiteHeader
+    }
+};
+</script>
+
 <style lang="scss">
+@import "@/assets/scss/styles.scss";
 </style>
